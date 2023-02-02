66af0ddad8131fe8337a600c2c0c629cc3588134
Manivannan0734 <114678714+Manivannan0734@users.noreply.github.com>
2023-02-02 23:13:52 +0530

    Add files via upload

---
+++ src/Components/Box2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Box1.css'
+const Whole = () => {
+  return (
+    <div>
+        <div className='bigBox'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Whole
